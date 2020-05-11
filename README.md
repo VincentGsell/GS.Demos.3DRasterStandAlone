14e46b63cde5c886096a8def94218f11cf9d62e8
Vincent Gsell <VincentOnGitHub@grids.systems>
2020-05-11 15:46:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GS.Demos.3DRasterStandAlone
+  3D raster exemple, all in one unit, learning aims
---
+++ delphi/D3DRaster.dpr
@@ -0,0 +1,18 @@
+program D3DRaster;
+
+uses
+  Vcl.Forms,
+  D3DRaster.fmain in 'D3DRaster.fmain.pas' {Form1},
+  GS.SoftwareRaster3D.ExempleStandAlone in '..\common\GS.SoftwareRaster3D.ExempleStandAlone.pas';
+
+{$R *.res}
+
+begin
+  {$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := true;
+  {$ENDIF}
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
